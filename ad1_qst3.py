c9845165d00165356dc10aa14f92f127fac8a07f
Alexandre Vitor <121124727+Allex4xandre@users.noreply.github.com>
2023-08-11 12:36:42 -0300

    Add files via upload
    Os seguintes arquivos são programas SIMPLES em Python para um trabalho de faculdade.
    
    O usuário insere uma quantidade N de números para que logo em seguida seja informado o menor e maior valor e a media dos pares e impares com precisão decimal requisitada no trabalho

---
+++ ad1_qst3.py
@@ -0,0 +1,19 @@
+valores=[[],[]]
+num=input('Digite um numero Inteiro(Z) maior que zero: ')
+conver = list(map(int, num.split()))
+
+for num in conver:
+
+    if num % 2 == 0:
+       valores[0].append(num)
+       med_par=sum(valores[0])/len(valores[0])
+    else:
+        valores[1].append(num)
+        med_imp = sum(valores[1]) / len(valores[1])
+menor_valor=min(valores[0]+valores[1])
+maior_valor=max(valores[0]+valores[1])
+
+print('O menor valor fornecido é o numero:',menor_valor)
+print('O maior valor fornecido é o numero:',maior_valor)
+print('A média dos pares é :%.1f ' %med_par)
+print('A média dos impares é:%.1f ' %med_imp)
